748b1fc10d4d4195627ff4a5a942ba0b465d6ad6
theempij <julian.chandra.hauck@gmail.com>
2022-07-25 10:00:27 +0200

    deploy.rb fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "nachhilfe"
+set :repo_url, "git@github.com:theempij/nachhilfe.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
